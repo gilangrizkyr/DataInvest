1384653e2fe8c624eae4752c07851dedd2282ecc
Gilang Rizky <gilangrizkyramadhan34@gmail.com>
2026-01-21 18:54:09 +0800

    update1

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * CI4 Index - Working Without mod_rewrite
- * Menggunakan $_GET['r'] untuk route, lalu memodifikasi REQUEST_URI
  */
 
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
@@ -10,15 +9,15 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
     chdir(FCPATH);
 }
 
-// Setup route sebelum CI4 boot
 if (isset($_GET['r'])) {
     $route = ltrim($_GET['r'], '/');
+    unset($_GET['r']);
+    $_SERVER['QUERY_STRING'] = '';
     $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $_SERVER['REQUEST_URI'] = $requestUri . '/' . $route;
     $_SERVER['PATH_INFO'] = '/' . $route;
 }
 
-// Boot CI4
 require dirname(__DIR__) . '/app/Config/Paths.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
