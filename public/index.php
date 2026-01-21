3f0fef2e61eedcad3c1ba32debcf3e005cc8edbc
Gilang Rizky <gilangrizkyramadhan34@gmail.com>
2026-01-21 18:57:11 +0800

    update12

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * CI4 Index - Working Without mod_rewrite
+ * Menggunakan ?r=/route untuk akses halaman
  */
 
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
@@ -9,15 +10,25 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
     chdir(FCPATH);
 }
 
+// Base path aplikasi
+$basePath = '/DataInvest/public';
+
+// Setup route dari query string ?r=/dashboard
 if (isset($_GET['r'])) {
     $route = ltrim($_GET['r'], '/');
     unset($_GET['r']);
     $_SERVER['QUERY_STRING'] = '';
+    
     $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $requestUri = str_replace($basePath . '/index.php', '', $requestUri);
+    if ($requestUri === $basePath) {
+        $requestUri = $basePath . '/';
+    }
     $_SERVER['REQUEST_URI'] = $requestUri . '/' . $route;
     $_SERVER['PATH_INFO'] = '/' . $route;
 }
 
+// Boot CI4
 require dirname(__DIR__) . '/app/Config/Paths.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
