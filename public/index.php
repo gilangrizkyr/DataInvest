4f32a201a112bc30530d5a44edaff9806b20b5a7
Gilang Rizky <gilangrizkyramadhan34@gmail.com>
2026-01-21 18:59:38 +0800

    update12

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
-/**
- * CI4 Index - Working Without mod_rewrite
- * Menggunakan ?r=/route untuk akses halaman
- */
+
+use CodeIgniter\Boot;
+use Config\Paths;
+
+$minPhpVersion = '8.1';
+if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'PHP version must be 8.1 or higher.';
+    exit(1);
+}
 
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
@@ -10,31 +16,12 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
     chdir(FCPATH);
 }
 
-// Base path aplikasi
-$basePath = '/DataInvest/public';
-
-// Setup route dari query string ?r=/dashboard
-if (isset($_GET['r'])) {
-    $route = ltrim($_GET['r'], '/');
-    unset($_GET['r']);
-    $_SERVER['QUERY_STRING'] = '';
-    
-    $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $requestUri = str_replace($basePath . '/index.php', '', $requestUri);
-    if ($requestUri === $basePath) {
-        $requestUri = $basePath . '/';
-    }
-    $_SERVER['REQUEST_URI'] = $requestUri . '/' . $route;
-    $_SERVER['PATH_INFO'] = '/' . $route;
-}
-
-// Boot CI4
 require dirname(__DIR__) . '/app/Config/Paths.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$paths = new \Config\Paths();
+$paths = new Paths();
 
 require $paths->systemDirectory . '/Boot.php';
 
-exit(\CodeIgniter\Boot::bootWeb($paths));
+exit(Boot::bootWeb($paths));
 
